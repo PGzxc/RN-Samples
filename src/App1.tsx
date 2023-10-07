b3353b55af9608fe6c226f8f57249d4e2285f094
PGzxc <zxc_developer@126.com>
2023-10-07 15:57:25 +0800

    samples

---
+++ src/App1.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import ButtonComponent from "./components/ButtonComponent";
+import FlatListComponent from "./components/FlatListComponent";
+import ImageComponent from "./components/ImageComponent";
+import ImageBackgroundComponent from "./components/ImageBackgroundComponent";
+import PressableComponent from "./components/PressableComponent";
+import ActivityIndicatorComponent from "./components/ActivityIndicatorComponent";
+import KeyboardAvoidingComponent from "./components/KeyboardAvoidingComponent";
+import ModalComponent from "./components/ModalComponent";
+import RefreshControlComponent from "./components/RefreshControlComponent";
+import ScrollViewComponent from "./components/ScrollViewComponent";
+import SectionListComponent from "./components/SectionListComponent";
+import StatusBarComponent from "./components/StatusBarComponent";
+import SwitchComponent from "./components/SwitchComponent";
+import TextComponent from "./components/TextComponent";
+import TextInputComponent from "./components/TextInputComponent";
+import TouchableHighlightComponent from "./components/TouchableHighlightComponent";
+import TouchableOpacityComponent from "./components/TouchableOpacityComponent";
+import ViewComponent from "./components/ViewComponent";
+import VirtualizedListComponent from "./components/VirtualizedListComponent";
+import DrawerLayoutAndroidComponent from "./components/androidcomponent/DrawerLayoutAndroidComponent";
+import TouchableNativeFeedbackAndroidComponent from "./components/androidcomponent/TouchableNativeFeedbackAndroidComponent";
+import InputAccessoryViewComponent from "./components/ioscomponent/InputAccessoryViewComponent";
+import SafeAreaViewComponent from "./components/ioscomponent/SafeAreaViewComponent";
+import ImageResizeModesComponent from "./props/ImageResizeModesComponent";
+import LayoutPropsComponent from "./props/LayoutPropsComponent";
+import ViewStylePropsComponent from "./props/ViewStylePropsComponent";
+import AccessibilityInfoExample from "./apis/AccessibilityInfoExample";
+import AlertExample from "./apis/AlertExample";
+import AnimatedExample from "./apis/AnimatedExample";
+import AnimatedValueXYExample from "./apis/AnimatedValueXYExample";
+import AppStateExample from "./apis/AppStateExample";
+import DimensionsExample from "./apis/DimensionsExample";
+import EasingDemo from "./apis/EasingDemo";
+import InteractionManagerFunctionComponentBasicExample from "./apis/InteractionManagerFunctionComponentBasicExample";
+import KeyboardExample from "./apis/KeyboardExample";
+import LayoutAnimationExample from "./apis/LayoutAnimationExample";
+import LinkingExample from "./apis/LinkingExample";
+import PanResponderExample from "./apis/PanResponderExample";
+import PixelRatioExample from "./apis/PixelRatioExample";
+import PlatformAPIExample from "./apis/PlatformAPIExample";
+import PlatformColorExample from "./apis/PlatformColorExample";
+import ShareExample from "./apis/ShareExample";
+import StyleSheetExample from "./apis/StyleSheetExample";
+import SystraceExample from "./apis/SystraceExample";
+import TransformsExample from "./apis/TransformsExample";
+import VibrationExample from "./apis/VibrationExample";
+import UseColorSchemeExample from "./apis/hook/UseColorSchemeExample";
+import UseWindowDimensionsExample from "./apis/hook/UseWindowDimensionsExample";
+import BackHandlerExample from "./apis/android/BackHandlerExample";
+import PermissionsAndroidExample from "./apis/android/PermissionsAndroidExample";
+import ToastAndroidAPIExample from "./apis/android/ToastAndroidAPIExample";
+import ActionSheetIOSExample from "./apis/ios/ActionSheetIOSExample";
+import SettingsExample from "./apis/ios/SettingsExample";
+
+function App1(): JSX.Element {
+    return (<ActivityIndicatorComponent />)
+
+}
+
+export default App1;
